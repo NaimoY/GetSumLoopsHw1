a2bb474c69ac97731b3f2acb93fb2175d5ccf449
NaimoY <naimowyusuf@gmail.com>
2025-02-22 23:53:20 -0800

    Cleaned up code

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
         int[] numArr1 = {100, 200, 300, 400, 500};
         int[] numArr2 = {11,22,33};
 
         int sum1 = getSum(numArr1);
         int sum2 = getSum(numArr2);
+
         System.out.println("NumArr1 Sum is: " + sum1);
         System.out.println("NumArr2 Sum is: " + sum2);
-
     }
 
     // nums = 100, 200, 300, 400
@@ -21,12 +20,6 @@ public class Main {
         }
         return currentSum;
     }
-    /*
-
-    // for each value in the array, update currentSum
-           //currentSum = currentSum + nums[i];
-          // currentSum += nums[i];
-     */
 }
 /*
 Assignment
